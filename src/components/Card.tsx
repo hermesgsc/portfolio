7a7cb759a20a962e80b4af9a287e4119d939e53d
hermesgsc <programadorgabriel22@gmail.com>
2025-03-13 10:11:33 -0300

    refactor: removing the border at mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ children, className }) => {
   return (
     <div
-      className={` border-[0.5px] gap-3 rounded-2xl max-w-[1080px] px-3 sm:px-6 ${
+      className={` gap-3 rounded-2xl max-w-[1080px] px-3 sm:px-6 ${
         className || ""
       }`}
     >
